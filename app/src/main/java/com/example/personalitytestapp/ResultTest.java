f27471410ab733026a634e553324adf4fac52e1a
Muhammad Rhomaedi <rhomaedycreator@gmail.com>
2023-04-25 23:02:04 +0700

    menambahkan tampilan Test kepribadian

--- app/src/main/java/com/example/personalitytestapp/ResultTest.java
+++ app/src/main/java/com/example/personalitytestapp/ResultTest.java
@@ -2,13 +2,27 @@ package com.example.personalitytestapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class ResultTest extends AppCompatActivity {
+
+    private Button backHome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result_test);
+        backHome = findViewById(R.id.btn_backHome);
+        backHome.setOnClickListener(v -> {
+            tabBackHome();
+        });
     }
+
+    private void tabBackHome() {
+        Intent intent = new Intent(getApplicationContext(), Home.class);
+        startActivity(intent);
+    }
+
 }
--- app/src/main/java/com/example/personalitytestapp/Test.java
+++ app/src/main/java/com/example/personalitytestapp/Test.java
@@ -2,13 +2,27 @@ package com.example.personalitytestapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class Test extends AppCompatActivity {
+
+    private Button finishTest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
+
+        finishTest = findViewById(R.id.btn_finish);
+        finishTest.setOnClickListener(v -> {
+            tabFinish();
+        });
+    }
+
+    private void tabFinish() {
+        Intent intent = new Intent(getApplicationContext(), ResultTest.class);
+        startActivity(intent);
     }
 }
